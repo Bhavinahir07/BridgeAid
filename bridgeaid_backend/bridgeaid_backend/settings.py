0fff8d2ca62efed0ab424ef454342e38b591d8fb
Bhavin <bhaviahir478@gmail.com>
2026-04-28 16:33:05 +0530

    updated settings for deployment3

--- bridgeaid_backend/bridgeaid_backend/settings.py
+++ bridgeaid_backend/bridgeaid_backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
 from datetime import timedelta
 import os
 
@@ -85,7 +84,14 @@ WSGI_APPLICATION = 'bridgeaid_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
